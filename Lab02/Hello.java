29d4e2ebc2054fc90726aa183b525b1488568431
Mike <Mike@10.117.58.134>
2018-10-25 15:01:37 -0700

    Added Information to Line 9

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("216596927 - This line was added by Dominic Tran.");
-		System.out.println("215197594 - This line was added by Michael Mun.");
+		System.out.println("215197594 - This line was added by Michael .");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
