787e9701500ddd99104aa2bbfd956576f9fff76e
our <our@WARCRAFT.ecs.csus.edu>
2018-10-30 13:36:30 -0700

    Name change

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -10,11 +10,11 @@ public class Hello {
 		System.out.println("216746830 - This line was added by Branden Balanay.");
 		System.out.println("218738872 - This line was added by Ly Nguyen.");
 		System.out.println("216189728 - This line was added by Minh Giang .");
-		System.out.println("219817313 - This line was added by Rong Ou.");
+		System.out.println("219817313 - This line was added by Rong OuX.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
-
+		
 }
 
 }
