21fe6850998c387951b03eefdaf655c29c515e17
our <our@WARCRAFT.ecs.csus.edu>
2018-10-30 13:31:10 -0700

    Line change

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -10,7 +10,7 @@ public class Hello {
 		System.out.println("216746830 - This line was added by Branden Balanay.");
 		System.out.println("218738872 - This line was added by Ly Nguyen.");
 		System.out.println("216189728 - This line was added by Minh Giang .");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219817313 - This line was added by Rong Ou.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
